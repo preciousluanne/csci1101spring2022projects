43ec0237ccc631f37fa1a043c49948f8c05115e6
Precious Ramos <precious.ramos01@utrgv.edu>
2022-04-20 16:37:56 -0500

    Add databases project
    This adds SQL queries and results to the database project.

---
+++ games-database/sandbox-create-update-delete-queries.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+	sandbox(int16_value, double_value, bool_value)
+VALUES
+	(304, 426.12345, 1)
+
+UPDATE 
+	sandbox
+SET
+	int64_value = -2897439283473, 
+	string_value = ':joy:'
+WHERE
+	id = 14
+
+DELETE FROM sandbox WHERE id = 14
